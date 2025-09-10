e64ddce18de62834309a006e87e9467007df39e4
Tymoteusz Zydkiewicz <tymek.zydkiewicz.szkola@gmail.com>
2025-09-10 15:16:16 +0200

    Ass 1 done

--- Entities/Comment.cs
+++ Entities/Comment.cs
@@ -1,6 +1,8 @@
 ﻿namespace Entities;
-
 public class Comment
 {
-    
+    public int Id { get; set; }
+    public string Content { get; set; }
+    public string UserId { get; set; }
+    public int PostId { get; set; }
 }
---
+++ InMemoryRepositories/PostInMemoryRepository.cs
@@ -0,0 +1,63 @@
+﻿using Entities;
+using RepositoryContracts;
+using RepositoryContracts;
+
+namespace InMemoryRepositories;
+
+public class PostInMemoryRepository: IPostRepository
+{
+    private readonly List<Post> posts = new();
+
+    public Task<Post> AddAsync(Post post)
+    {
+        post.Id = posts.Any()
+            ? posts.Max(p => p.Id) + 1
+            : 1;
+        posts.Add(post);
+        return Task.FromResult(post);
+    }
+    
+    public Task UpdateAsync(Post post)
+    {
+        var existingPost = posts.SingleOrDefault(p => p.Id == post.Id);
+        if (existingPost == null)
+        {
+            throw new InvalidOperationException($"Post with ID '{post.Id}' not found");
+        }
+
+        posts.Remove(existingPost);
+        posts.Add(post);
+        return Task.CompletedTask;
+    }
+    public Task DeleteAsync(int id)
+    {
+        var postToRemove = posts.SingleOrDefault(p => p.Id == id);
+        if (postToRemove == null)
+        {
+            throw new InvalidOperationException($"Post with ID '{id}' not found");
+        }
+
+        posts.Remove(postToRemove);
+        return Task.CompletedTask;
+    }
+    public Task<Post> GetSingleAsync(int id)
+    {
+        var post = posts.SingleOrDefault(p => p.Id == id);
+        if (post == null)
+        {
+            throw new InvalidOperationException($"Post with ID '{id}' not found");
+        }
+
+        return Task.FromResult(post);
+    }
+    public IQueryable<Post> GetManyAsync()
+    {
+        return posts.AsQueryable();
+    }
+
+    
+    
+
+    
+    
+}
