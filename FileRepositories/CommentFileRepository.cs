fe2e64b3d888f1c3f6e895986b290e0c743215d0
Tymoteusz Zydkiewicz <tymek.zydkiewicz.szkola@gmail.com>
2025-09-23 14:37:05 +0200

    ass 3 pt 1

---
+++ FileRepositories/CommentFileRepository.cs
@@ -0,0 +1,80 @@
+﻿using System.Text.Json;
+using Entities;
+
+namespace FileRepositories;
+
+public class CommentFileRepository
+{  
+    private readonly string filePath = "comments.json";
+
+    public CommentFileRepository()
+    {
+        if (!File.Exists(filePath))
+        {
+            File.WriteAllText(filePath, "[]");
+        }
+    }
+    public async Task<Comment> AddAsync(Comment comment)
+    {
+        string commentsAsJason = await File.ReadAllTextAsync(filePath);
+        List<Comment> comments = JsonSerializer.Deserialize<List<Comment>>(commentsAsJason);
+        int maxId = comments.Count > 0 ? comments.Max(c => c.Id) : 0;
+        comment.Id = maxId + 1;
+        comments.Add(comment);
+        commentsAsJason = JsonSerializer.Serialize(comments);
+        await File.WriteAllTextAsync(filePath, commentsAsJason);
+        return comment;
+    }
+    public async Task UpdateAsync(Comment comment)
+    {
+        string commentsAsJson = await File.ReadAllTextAsync(filePath);
+        List<Comment> comments = JsonSerializer.Deserialize< List < Comment >> (commentsAsJson);
+        
+        Comment? existingComment = comments.SingleOrDefault(c => c.Id == comment.Id);
+        if (existingComment is null)
+        {
+            throw new KeyNotFoundException($"Comment with ID '{comment.Id}' not found");
+        }
+
+        comment.Id = existingComment.Id;
+        comments.Remove(existingComment);
+        comments.Add(comment);
+        commentsAsJson = JsonSerializer.Serialize(comments);
+        await File.WriteAllTextAsync(filePath, commentsAsJson);
+    }
+    public async Task DeleteAsync(int id)
+    {
+        string commentsAsJson = await File.ReadAllTextAsync(filePath);
+        List<Comment> comments = JsonSerializer.Deserialize< List <Comment>> (commentsAsJson);
+        
+        var commentToRemove = comments.SingleOrDefault(c => c.Id == id);
+        if (commentToRemove is null)
+        {
+            throw new KeyNotFoundException($"Comment with ID '{id}' not found");
+        }
+        comments.Remove(commentToRemove);
+        commentsAsJson = JsonSerializer.Serialize(comments);
+        await File.WriteAllTextAsync(filePath, commentsAsJson);
+
+    }
+    public async Task<Comment> GetSingleAsync(int id)
+    {
+        string commentsAsJson = await File.ReadAllTextAsync(filePath);
+        List<Comment> comments = JsonSerializer.Deserialize< List <Comment>> (commentsAsJson);
+        
+        var comment = comments.SingleOrDefault(c => c.Id == id);
+        if (comment is null)
+        {
+            throw new KeyNotFoundException($"Comment with ID '{id}' not found");
+        }
+        return comment;
+    }
+
+    public IQueryable<Comment> GetMany()
+    {
+        string commentsAsJson = File.ReadAllTextAsync(filePath).Result;
+        List<Comment> comments = 
+            JsonSerializer.Deserialize<List<Comment>>(commentsAsJson)!;
+        return comments.AsQueryable();
+    }
+}
