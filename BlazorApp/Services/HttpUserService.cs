692c0b0e491149203c97d5f6698d948327f51b6f
Tymoteusz Zydkiewicz <tymek.zydkiewicz.szkola@gmail.com>
2025-11-05 15:22:09 +0100

    ASS 5 pt.2

---
+++ BlazorApp/Services/HttpUserService.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using System.Net.Http.Json; 
+using ApiContracts; 
+using System.Threading.Tasks;
+
+public class HttpUserService : IUserService
+{
+    private readonly HttpClient client;
+    
+    public HttpUserService(HttpClient client)
+    {
+        this.client = client;
+    }
+    public async Task<ReturnUserDto> CreateAsync(CreateUserDto request)
+    {
+        HttpResponseMessage httpResponse = await client.PostAsJsonAsync("users", request);
+
+        
+        string response = await httpResponse.Content.ReadAsStringAsync();
+        
+        if (!httpResponse.IsSuccessStatusCode)
+        {
+            //
+            throw new Exception(response);
+        }
+
+        
+        var user = JsonSerializer.Deserialize<ReturnUserDto>(
+            response,
+            new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+        
+        return user!;
+    }
+}
---
+++ BlazorApp/Services/IUserService.cs
@@ -0,0 +1,7 @@
+﻿using System.Threading.Tasks;
+using ApiContracts;
+
+public interface IUserService
+{
+    Task<ReturnUserDto> CreateAsync(CreateUserDto dto);
+}
